dfb53da7790bb40fd0969524eb0e8769578f88e8
Mokhtaar <mokh.first@gmail.com>
2025-01-21 21:20:45 +0200

    add debounce hook with callback function

---
+++ app/hooks/common/use-debounce.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+export function useDebounce<T>(
+  value: T,
+  delay: number,
+  callback?: (value: T) => void
+): T {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedValue(value);
+      callback?.(value);
+    }, delay);
+
+    return () => clearTimeout(timer);
+  }, [value, delay, callback]);
+
+  return debouncedValue;
+}
